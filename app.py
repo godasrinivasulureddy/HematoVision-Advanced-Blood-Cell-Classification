383aee51ca4cba270c6d5885263e9ef785ed6921
Srinu Reddy <167775970+godasrinivasulureddy@users.noreply.github.com>
2025-11-15 13:19:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -129,5 +129,6 @@ def uploaded_file(filename):
 
 
 if __name__ == "__main__":
-    # For local testing only. In production use gunicorn/uvicorn.
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
